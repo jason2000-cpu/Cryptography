05eba3bbd52e941ed7fac3f1b355eceb3f5aad97
jack2000-cpu <jasonmwangi2000@gmail.com>
2022-07-08 16:55:24 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cryptography
+
+This App takes in a text or a group of text through a HTML input element and transforms it to an unreadable form
+i.e cipher text therefore achieving encryption
---
+++ assets/Cipher.js
@@ -0,0 +1,55 @@
+
+let results = document.querySelector("#output")
+
+
+
+function rot13(message) {
+
+  let alphabets = "abcdefghijklmnopqrstuvwxyz";
+  let specChar = "!@#$%^&*()_+~=-".split('')
+  let alphaArr = alphabets.split("");
+  let messageArr = message.split("");
+  let cipherArr =[];
+  for (i = 0; i < messageArr.length; i++) {
+    
+    let index = alphaArr.indexOf(messageArr[i]);
+    console.log(index)
+
+    if(alphaArr.includes(messageArr[i].toLowerCase())){
+         if (index === -1){
+       index = alphaArr.indexOf(messageArr[i].toLowerCase());
+       index =index+13;
+       if(index > 26){
+        cipherArr.push(specChar[Math.floor(Math.random()*specChar.length)])
+        cipherArr.push(alphaArr[index-26].toLocaleUpperCase())
+      }else{
+        cipherArr.push(specChar[Math.floor(Math.random()*specChar.length)])
+        cipherArr.push(alphaArr[index].toUpperCase())
+      }
+
+     }else{
+      index = index+13
+        if(index > 26){
+          cipherArr.push(specChar[Math.floor(Math.random()*specChar.length)])
+          cipherArr.push(alphaArr[index-26])
+        }else{
+          cipherArr.push(alphaArr[index])
+        }
+  }
+
+    }else{
+      cipherArr.push(specChar[Math.floor(Math.random()*specChar.length)])
+      cipherArr.push(messageArr[i])
+    }
+     
+  
+     }
+
+    
+     results.innerText = cipherArr.join("");
+     return cipherArr.join('')
+     
+}
+
+
+
